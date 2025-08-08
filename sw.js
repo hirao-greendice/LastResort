380b24482e3e4ea25a25b49c3d0468a3be6c6a91
hirao-greendice <s.hirao@greendice.co.jp>
2025-08-08 17:16:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // - Supports Range requests for cached media
 // - Versioned caches; bump CACHE_VERSION when replacing media with same filenames
 
-const CACHE_VERSION = 'v5-20250808';
+const CACHE_VERSION = 'v6-20250808';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const MEDIA_CACHE = `media-${CACHE_VERSION}`;
 
